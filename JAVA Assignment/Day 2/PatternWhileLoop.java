491927c1252532209039f687f7512f7d2572fe6f
Sathish Kumar S <sathisuh@gmail.com>
2025-07-24 10:19:30 +0530

    Update PatternWhileLoop.java

--- JAVA Assignment/Day 2/PatternWhileLoop.java
+++ JAVA Assignment/Day 2/PatternWhileLoop.java
@@ -1,24 +1,21 @@
-public class PatternWhileLoop {
+public class PatternWhileLoop{
     public static void main(String[] args) {
-        int num = 1;
-        int i = 1;
+        int n = 3; // Peak row with 3 W's
 
-        while (i <= 5) {
-            int j = 1;
-            while (j <= i) {
-                System.out.print(num + " ");
-                num++;
-                j++;
+        // Upper half (1 to 3)
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("W ");
             }
-
-            int k = 1;
-            while (k <= i) {
-                System.out.print("w ");
-                k++;
-            }
-
             System.out.println();
-            i++;
+        }
+
+        // Lower half (2 to 1)
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("W ");
+            }
+            System.out.println();
         }
     }
 }
